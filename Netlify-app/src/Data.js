5515657bd2477952dc3779caeaa8ce6d4eab71cd
Sasa Nedic <svetasmak0@gmail.com>
2024-03-15 00:52:17 +0100

    Create style for User component

--- Netlify-app/src/Data.js
+++ Netlify-app/src/Data.js
@@ -2,7 +2,7 @@ const reviews = [
   {
     id: 1,
     name: "susan smith",
-    job: "web developer",
+    job: "Web developer",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
     text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
@@ -10,7 +10,7 @@ const reviews = [
   {
     id: 2,
     name: "anna johnson",
-    job: "web designer",
+    job: "Web designer",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
     text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
@@ -18,7 +18,7 @@ const reviews = [
   {
     id: 3,
     name: "peter jones",
-    job: "intern",
+    job: "Intern",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
     text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
@@ -26,7 +26,7 @@ const reviews = [
   {
     id: 4,
     name: "bill anderson",
-    job: "the boss",
+    job: "The boss",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
     text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
