34877c81fa14a65968efa7db57199463a0c093e5
Sasa Nedic <svetasmak0@gmail.com>
2024-03-15 19:44:49 +0100

    Create state

--- Netlify-app/src/App.jsx
+++ Netlify-app/src/App.jsx
@@ -1,8 +1,15 @@
+import { useState } from "react"
 import reviews from "./Data"
 import User from "./User"
 
 function App() {
-
+const [user, setUser] = useState(1)
+const nextUser = () => {
+  setUser((prevUser) => prevUser + 1)
+}
+const prevUser = () => {
+  setUser((prevUser) => prevUser -1)
+}
   return (
     <section className="flex  justify-center mt-20">
       <User data={reviews[0]}/>
